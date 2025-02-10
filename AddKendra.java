1c75455837244e9067488b49d3b4e27869179d3c
Navneetpatid <patidarnavneet580@gmail.com>
2025-02-10 18:59:30 +0530

    Update AddKendra.java

--- AddKendra.java
+++ AddKendra.java
@@ -1,49 +1,20 @@
-try {
-    throw new Exception("Error while fetching gcloud output response");
-} catch (Exception e) {
-    println("Error while fetching gcloud output response" + e.getMessage());
-}
+import java.nio.file.*
 
-String filename = "mycsv.csv";
-println "Filename for writing: ${filename}";
-println "Filename for content file: ${contentFile}";
-writeFile file: "./Report/${filename}", text: "${contentFile}";
+String filename = "mycsv.csv"
+String filePath = "./Report/${filename}"
 
-println "Write file executed successfully - send mail list ${email_reciever}";
-
-String responseBody = readFile encoding: 'UTF-8', file: "./Report/${filename}";
-println "Response body: ${responseBody}";
-println "Read file executed successfully";
-
-String tempworkspaceName1 = "${workspace_name}";
-String tempworkspaceName = tempworkspaceName1.replace("-DEV", "");
-
-println "Sending Email to Recipients ${filename}";
-
-String message = """Hi Team,
-
-This is an auto-generated mail for ${tempworkspaceName}.
-
-Please find the attached report for Workspace, Services, Routes, and Plugins.
-
-For any queries, please reach out to Team.
-""";
-
-def html_body = sh(script: "cat test-output/emailable-report.html", returnStdout: true).trim();
-message += "\n\nThanks\n";
-
-emailext(
-    to: "${email_reciever}",
-    subject: "Admin API Pipeline Report - ${tempworkspaceName}",
-    attachmentsPattern: "./Report/${filename}",
-    body: "${html_body}",
-    mimeType: 'text/html'
-);
-
-def CP() {
-    credentials {
-        usernamePassword {
-            credentialsId: kongDeploymentAccountCredID
+if (Files.exists(Paths.get(filePath))) {
+    println "File exists: ${filePath}"
+    
+    // Send Email
+    emailext(
+        to: "navneet.patidar@noexternalmail.hsbc.com",
+        subject: "License Report",
+        attachmentsPattern: filePath,
+        body: "Please find the attached report.",
+        mimeType: "text/html"
+    )
+    println "Email sent successfully."
+} else {
+    println "File not found: ${filePath}. Email not sent."
         }
-    }
-                                    }
