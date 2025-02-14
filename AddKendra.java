405592c711d2d4eae4f8ec8e33dee93b044443c4
Navneetpatid <patidarnavneet580@gmail.com>
2025-02-14 12:53:03 +0530

    Update AddKendra.java

--- AddKendra.java
+++ AddKendra.java
@@ -1,8 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Base64;
 
-public class URLChecker {
+public class PostRequestExample {
     public static void main(String[] args) {
         try {
             // API URL
@@ -10,21 +11,32 @@ public class URLChecker {
             URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            // Set request method to GET
-            conn.setRequestMethod("GET");
-
-            // Replace with valid credentials
-            String username = "your_username";
-            String password = "your_password";
-            String encodedAuth = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
-            conn.setRequestProperty("Authorization", "Basic " + encodedAuth);
-
-            // Replace with actual business details
-            conn.setRequestProperty("X-Custom", "yourBusinessUnit|yourBusinessAppId");
+            // Setting request method and headers
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Basic g+Ojx5b3VyIHN1cnZpY2UgYWNjb29yd29yZD4=");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("X-Custom", "<your businessUnit>|<your businessAppId>");
 
             // Get response code
             int responseCode = conn.getResponseCode();
             System.out.println("Response Code: " + responseCode);
+
+            // Read response if successful
+            if (responseCode >= 200 && responseCode < 300) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Print the response
+                System.out.println("Response: " + response.toString());
+            } else {
+                System.out.println("Failed to connect. Response Code: " + responseCode);
+            }
 
             // Close connection
             conn.disconnect();
