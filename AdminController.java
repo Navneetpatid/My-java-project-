655e9e3e16701e21533f0dc2e31394d107498b4b
Navneetpatid <patidarnavneet580@gmail.com>
2025-03-05 15:16:07 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -1,28 +1,31 @@
-import groovy.json.JsonSlurper
+response = sh(
+    script: """
+        set +x;
+        curl --silent -k -b cookie.txt --location --request GET https://${CP}/admin/license/report
+    """,
+    returnStdout: true
+)
 
-def filename = "${filename}_license.json"  // Use the same filename as in writeFile
+echo "https://${CP}/admin/license/report"
+echo "getworkspaceservices response starts"
+
+echo "${response}"
+
+// Define the filename
+String filename = "${filename}_license.json"
+
+// Write the response to a JSON file
+writeFile file: filename, text: response
+echo "getworkspaceservices response ends"
 
 // Read the file
-def file = new File(filename)
-
+File file = new File(filename)
 if (file.exists()) {
-    def fileContents = file.text
-    println "File Contents:\n${fileContents}"
-
-    // Parse JSON
-    def jsonSlurper = new JsonSlurper()
-    def jsonResponse = jsonSlurper.parseText(fileContents)
-
-    println "Parsed JSON Data:"
-    println jsonResponse
-
-    // If JSON contains data array, iterate over it
-    if (jsonResponse?.data) {
-        jsonResponse.data.each { service ->
-            println "Service Name: ${service.name}"
-            println "Service ID: ${service.id}"
-        }
-    }
+    String fileContent = file.text
+    echo "Read file executed successfully"
+    echo "File Content:\n${fileContent}"
 } else {
-    println "File not found: ${filename}"
+    echo "File not found: ${filename}"
 }
+
+return 0;
