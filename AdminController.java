afc843b6a91fe9238ec58c9486824001a04614fb
Navneetpatid <patidarnavneet580@gmail.com>
2025-03-05 15:21:54 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -1,16 +1,3 @@
-response = sh(
-    script: """
-        set +x;
-        curl --silent -k -b cookie.txt --location --request GET https://${CP}/admin/license/report
-    """,
-    returnStdout: true
-)
-
-echo "https://${CP}/admin/license/report"
-echo "getworkspaceservices response starts"
-
-echo "${response}"
-
 // Define the filename
 String filename = "${filename}_license.json"
 
@@ -18,14 +5,18 @@ String filename = "${filename}_license.json"
 writeFile file: filename, text: response
 echo "getworkspaceservices response ends"
 
-// Read the file
-File file = new File(filename)
-if (file.exists()) {
-    String fileContent = file.text
-    echo "Read file executed successfully"
-    echo "File Content:\n${fileContent}"
-} else {
-    echo "File not found: ${filename}"
+// Read the file correctly
+try {
+    def file = new File(filename)
+    if (file.exists()) {
+        String fileContent = file.text
+        echo "Read file executed successfully"
+        echo "File Content:\n${fileContent}"
+    } else {
+        echo "File not found: ${filename}"
+    }
+} catch (Exception e) {
+    echo "Error reading file: ${e.getMessage()}"
 }
 
 return 0;
