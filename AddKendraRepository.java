c2cd3f9480031309d7ce0ac3469c273fd8905777
Navneetpatid <patidarnavneet580@gmail.com>
2025-03-18 22:06:24 +0530

    Update AddKendraRepository.java

--- AddKendraRepository.java
+++ AddKendraRepository.java
@@ -1,16 +1,26 @@
-package com.hsbc.hap.cdr.dto;
+public KongApiResponse getCERData(String engagementId, String region, String platform, String environment) {
+    KongApiResponse responseDTO = new KongApiResponse();
 
-import lombok.*;
+    // Fetch cp_master data by region
+    List<CpMaster> cpMastersByRegion = cpMasterDetailsDao.findById_Region(region);
 
-import java.util.List;
+    // Fetch cp_master data by platform and environment
+    List<CpMaster> cpMastersByPlatformAndEnv = cpMasterDetailsDao.findById_PlatformAndId_Environment(platform, environment);
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CERResponseDTO {
-    private CpMaster cpMaster;
-    private List<EngagementPlugin> engagementPlugins;
-    private EngagementTarget engagementTarget;
-    private List<WorkspaceTarget> workspaceTargets;
+    // Combine results (Optional: Handle duplicates or conflicts)
+    Set<CpMaster> uniqueCpMasters = new HashSet<>();
+    uniqueCpMasters.addAll(cpMastersByRegion);
+    uniqueCpMasters.addAll(cpMastersByPlatformAndEnv);
+
+    if (!uniqueCpMasters.isEmpty()) {
+        responseDTO.setCpMaster(new ArrayList<>(uniqueCpMasters));
+    } else {
+        responseDTO.setErrors("No data found in cp_master for given parameters");
+    }
+
+    // Fetch engagement plugin data
+    List<EngagementPluginDetail> engagementPlugins = engagementPluginDetailsDao.findByEngagementId(engagementId);
+    responseDTO.setEngagementPlugins(engagementPlugins);
+
+    return responseDTO;
 }
