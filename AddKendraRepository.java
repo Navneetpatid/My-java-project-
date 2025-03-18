e2205fb3b2090c31a8151bfe25019ddafbede33e
Navneetpatid <patidarnavneet580@gmail.com>
2025-03-18 20:11:03 +0530

    Update AddKendraRepository.java

--- AddKendraRepository.java
+++ AddKendraRepository.java
@@ -1,28 +1,21 @@
-package com.hsbc.hap.cdr.controller;
+package com.hsbc.hap.cdr.dto;
 
-import com.hsbc.hap.cdr.dto.KongApiResponseDTO;
-import com.hsbc.hap.cdr.service.CERService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import lombok.*;
 
-@RestController
-@RequestMapping("/api/kong")
-public class CERController {
+import java.util.List;
 
-    private final CERService cerService;
-
-    public CERController(CERService cerService) {
-        this.cerService = cerService;
-    }
-
-    @GetMapping("/getData")
-    public ResponseEntity<KongApiResponseDTO> getKongApiData(
-        @RequestParam String engagementId,
-        @RequestParam String region,
-        @RequestParam String platform,
-        @RequestParam String environment
-    ) {
-        KongApiResponseDTO responseDTO = cerService.getKongApiData(engagementId, region, platform, environment);
-        return ResponseEntity.ok(responseDTO);
-    }
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KongApiResponseDTO {
+    private String success;
+    private String errors;
+    private String workspace;
+    private String cpUrl;
+    private List<String> mandatoryPlugins;
+    private String dpHost;
+    private String gbgf;
+    private String dmzLb;
+    private String logs;
 }
