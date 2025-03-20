75de48eea096df5fc1606c23d8068ff09e489cce
Navneetpatid <patidarnavneet580@gmail.com>
2025-03-20 19:10:02 +0530

    Update AddKendraServiceImplTest.java

--- AddKendraServiceImplTest.java
+++ AddKendraServiceImplTest.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/hap-cdr")
 public class HapCDRController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HapCDRController.class);
 
     @Autowired
     private HapCDRService hapCDRService;
@@ -20,7 +25,12 @@ public class HapCDRController {
             @RequestParam(required = true) String engagementId,
             @RequestParam(required = true) String workspace) {
 
+        LOGGER.info("Validating workspace for Engagement ID: {} and Workspace: {}", engagementId, workspace);
+
         List<Map<String, Object>> responseList = hapCDRService.validateWorkspaceForEngagement(engagementId, workspace);
+
+        LOGGER.info("Validation completed for Engagement ID: {}, Response: {}", engagementId, responseList);
+
         return ResponseEntity.ok(responseList);
     }
 }
