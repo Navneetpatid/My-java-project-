0ad87e2e1712f6f4c3f2c4bbb09a5d1565beba29
Navneetpatid <patidarnavneet580@gmail.com>
2025-03-20 12:46:36 +0530

    Update AddKendraResponse.java

--- AddKendraResponse.java
+++ AddKendraResponse.java
@@ -4,19 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ValidationResponseDTO {
-    private boolean success;
-    private String errors;
-    private List<String> workspace;
-    private String cpUrl;
-    private List<String> mandatoryPlugins;
-    private String dpHost;
-    private String gbgf;
-    private String dmzLb;
-    private String logs;
+public class ErrorResponseDTO {
+    private String message;
 }
