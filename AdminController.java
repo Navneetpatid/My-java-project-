86611d5694d35e1bdaf8f89328fbe5549137f883
Navneetpatid <patidarnavneet580@gmail.com>
2025-04-01 15:36:35 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -1,40 +1,65 @@
--- 1. Update HK IKP Dev record (currently has NULL URL)
+-- 1. Update HK DEV environment
 UPDATE public.cp_master
 SET 
-    platform = 'GCP',  -- Changing from IKP to GCP
-    environment = 'DEV',  -- Ensuring proper case
+    platform = 'GCP',
+    environment = 'DEV',
     cp_admin_api_url = 'https://kcphk-dev.hsbc-11383538-kongcphk10-dev.dev.gcp.cloud.hk.hsbc',
+    created_date = COALESCE(created_date, CURRENT_TIMESTAMP),
     updated_date = CURRENT_TIMESTAMP
-WHERE region = 'HK' AND platform = 'IKP' AND environment = 'Dev';
+WHERE UPPER(region) = 'HK' 
+  AND UPPER(environment) = 'DEV';
 
--- 2. Update UK IKP Dev record (currently has NULL URL)
+-- 2. Update HK PPD environment
 UPDATE public.cp_master
 SET 
-    platform = 'GCP',  -- Changing from IKP to GCP
-    environment = 'DEV',  -- Ensuring proper case
-    cp_admin_api_url = 'https://kcpuk-dev.hsbc-11382986-kongcpuk10-dev.dev.gcp.cloud.uk.hsbc',
-    updated_date = CURRENT_TIMESTAMP
-WHERE region = 'UK' AND platform = 'IKP' AND environment = 'Dev';
-
--- 3. Update UK GKE Dev record (has existing URL but needs standardization)
-UPDATE public.cp_master
-SET 
-    platform = 'GCP',  -- Changing from GKE to GCP
-    cp_admin_api_url = 'https://kcpuk-dev.hsbc-11382986-kongcpuk10-dev.dev.gcp.cloud.uk.hsbc',
-    updated_date = CURRENT_TIMESTAMP
-WHERE region = 'UK' AND platform = 'GKE' AND environment = 'Dev';
-
--- 4. UK GCP PRD record is correct - no update needed
-
--- Additional records for PPD environments (not shown but mentioned in previous requests)
-UPDATE public.cp_master
-SET 
+    platform = 'GCP',
+    environment = 'PPD',
     cp_admin_api_url = 'https://kcphk-ppd.hsbc-11383538-kongcphk90-ppd.ppd.gcp.cloud.hk.hsbc',
+    created_date = COALESCE(created_date, CURRENT_TIMESTAMP),
     updated_date = CURRENT_TIMESTAMP
-WHERE region = 'HK' AND platform = 'GCP' AND environment = 'PPD';
+WHERE UPPER(region) = 'HK' 
+  AND UPPER(environment) = 'PPD';
 
+-- 3. Update HK PRD environment
 UPDATE public.cp_master
 SET 
-    cp_admin_api_url = 'https://kcpuk-ppd.hsbc-11382986-kongcpuk90-ppd.ppd.gcp.cloud.uk.hsbc',
+    platform = 'GCP',
+    environment = 'PRD',
+    cp_admin_api_url = 'https://kcphk-prod.hsbc-11383538-kongcphk-prod.prod.gcp.cloud.hk.hsbc',
+    created_date = COALESCE(created_date, CURRENT_TIMESTAMP),
     updated_date = CURRENT_TIMESTAMP
-WHERE region = 'UK' AND platform = 'GCP' AND environment = 'PPD';
+WHERE UPPER(region) = 'HK' 
+  AND UPPER(environment) = 'PRD';
+
+-- 4. Update UK DEV environment
+UPDATE public.cp_master
+SET 
+    platform = 'GCP',
+    environment = 'DEV',
+    cp_admin_api_url = 'https://kcpuk-dev.hsbc-11382986-kongcpuk10-dev.dev.gcp.cloud.uk.hsbc',
+    created_date = COALESCE(created_date, CURRENT_TIMESTAMP),
+    updated_date = CURRENT_TIMESTAMP
+WHERE UPPER(region) = 'UK' 
+  AND UPPER(environment) = 'DEV';
+
+-- 5. Update UK PPD environment
+UPDATE public.cp_master
+SET 
+    platform = 'GCP',
+    environment = 'PPD',
+    cp_admin_api_url = 'https://kcpuk-ppd.hsbc-11382986-kongcpuk90-ppd.ppd.gcp.cloud.uk.hsbc',
+    created_date = COALESCE(created_date, CURRENT_TIMESTAMP),
+    updated_date = CURRENT_TIMESTAMP
+WHERE UPPER(region) = 'UK' 
+  AND UPPER(environment) = 'PPD';
+
+-- 6. Update UK PRD environment
+UPDATE public.cp_master
+SET 
+    platform = 'GCP',
+    environment = 'PRD',
+    cp_admin_api_url = 'https://kcpuk-prod.hsbc-11382986-kongcpuk-prod.prod.gcp.cloud.uk.hsbc',
+    created_date = COALESCE(created_date, CURRENT_TIMESTAMP),
+    updated_date = CURRENT_TIMESTAMP
+WHERE UPPER(region) = 'UK' 
+  AND UPPER(environment) = 'PRD';
