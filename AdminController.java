a2b6646d13d91963ae5671daa3c93c48ab353b7a
Navneetpatid <patidarnavneet580@gmail.com>
2025-04-01 16:12:36 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -1,6 +1,5 @@
--- Corrected INSERT statements for HK region
-INSERT INTO public.cp_master (region, platform, environment, cp_sdmdn_api_url)  
-SELECT 'HK', 'GCP', 'PRD', 'https://kcphk-prod.hsbc-11383538-kongcphk-prod.prod.gcp.cloud.hk.hsbc'  
+-- Corrected SELECT test query (from first part of your image)
+SELECT 'HK', 'GCP', 'PRD', 'https://kcphk-prod.hsbc-11383538-kongcphk-prod.gcp.cloud.hk.hsbc'  
 WHERE NOT EXISTS (  
     SELECT 1 FROM public.cp_master  
     WHERE region = 'HK' AND platform = 'GCP' AND environment = 'PRD'  
@@ -12,4 +11,12 @@ SELECT 'UK', 'GCP', 'PRD', 'https://kcpuk-prod.hsbc-11382986-kongcpuk-prod.prod.
 WHERE NOT EXISTS (  
     SELECT 1 FROM public.cp_master  
     WHERE region = 'UK' AND platform = 'GCP' AND environment = 'PRD'  
+);
+
+-- For HK region (if you need to insert this too)
+INSERT INTO public.cp_master (region, platform, environment, cp_sdmdn_api_url)  
+SELECT 'HK', 'GCP', 'PRD', 'https://kcphk-prod.hsbc-11383538-kongcphk-prod.gcp.cloud.hk.hsbc'  
+WHERE NOT EXISTS (  
+    SELECT 1 FROM public.cp_master  
+    WHERE region = 'HK' AND platform = 'GCP' AND environment = 'PRD'  
 );
