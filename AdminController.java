aba0ca8fe18189818f4bbcd5e96a657ceb3bcbe6
Navneetpatid <patidarnavneet580@gmail.com>
2025-04-01 15:28:07 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -1,23 +1,35 @@
--- HK DEV
-INSERT INTO cp_master (region, platform, environment, cp_admin_api_url, created_date, updated_date)
-VALUES ('HK', 'GCP', 'DEV', 'https://kcphk-dev.hsbc-11383538-kongcphk10-dev.dev.gcp.cloud.hk.hsbc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- Update Hong Kong DEV environment
+UPDATE public.cp_master
+SET cp_admin_api_url = 'https://kcphk-dev.hsbc-11383538-kongcphk10-dev.dev.gcp.cloud.hk.hsbc',
+    updated_date = CURRENT_TIMESTAMP
+WHERE region = 'HK' AND platform = 'GCP' AND environment = 'DEV';
 
--- UK DEV
-INSERT INTO cp_master (region, platform, environment, cp_admin_api_url, created_date, updated_date)
-VALUES ('UK', 'GCP', 'DEV', 'https://kcpuk-dev.hsbc-11382986-kongcpuk10-dev.dev.gcp.cloud.uk.hsbc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- Update Hong Kong PPD environment
+UPDATE public.cp_master
+SET cp_admin_api_url = 'https://kcphk-ppd.hsbc-11383538-kongcphk90-dev.dev.gcp.cloud.hk.hsbc',
+    updated_date = CURRENT_TIMESTAMP
+WHERE region = 'HK' AND platform = 'GCP' AND environment = 'PPD';
 
--- HK PPD
-INSERT INTO cp_master (region, platform, environment, cp_admin_api_url, created_date, updated_date)
-VALUES ('HK', 'GCP', 'PPD', 'https://kcphk-ppd.hsbc-11383538-kongcphk90-dev.dev.gcp.cloud.hk.hsbc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- Update Hong Kong PRD environment
+UPDATE public.cp_master
+SET cp_admin_api_url = 'https://kcphk-prod.hsbc-11383538-kongcphk-prod.prod.gcp.cloud.hk.hsbc',
+    updated_date = CURRENT_TIMESTAMP
+WHERE region = 'HK' AND platform = 'GCP' AND environment = 'PRD';
 
--- UK PPD
-INSERT INTO cp_master (region, platform, environment, cp_admin_api_url, created_date, updated_date)
-VALUES ('UK', 'GCP', 'PPD', 'https://kcpuk-ppd.hsbc-11382986-kongcpuk90-dev.dev.gcp.cloud.uk.hsbc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- Update UK DEV environment
+UPDATE public.cp_master
+SET cp_admin_api_url = 'https://kcpuk-dev.hsbc-11382986-kongcpuk10-dev.dev.gcp.cloud.uk.hsbc',
+    updated_date = CURRENT_TIMESTAMP
+WHERE region = 'UK' AND platform = 'GCP' AND environment = 'DEV';
 
--- HK PRD
-INSERT INTO cp_master (region, platform, environment, cp_admin_api_url, created_date, updated_date)
-VALUES ('HK', 'GCP', 'PRD', 'https://kcphk-prod.hsbc-11383538-kongcphk-prod.prod.gcp.cloud.hk.hsbc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- Update UK PPD environment
+UPDATE public.cp_master
+SET cp_admin_api_url = 'https://kcpuk-ppd.hsbc-11382986-kongcpuk90-dev.dev.gcp.cloud.uk.hsbc',
+    updated_date = CURRENT_TIMESTAMP
+WHERE region = 'UK' AND platform = 'GCP' AND environment = 'PPD';
 
--- UK PRD
-INSERT INTO cp_master (region, platform, environment, cp_admin_api_url, created_date, updated_date)
-VALUES ('UK', 'GCP', 'PRD', 'https://kcpuk-prod.hsbc-11382986-kongcpuk-prod.prod.gcp.cloud.uk.hsbc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- Update UK PRD environment
+UPDATE public.cp_master
+SET cp_admin_api_url = 'https://kcpuk-prod.hsbc-11382986-kongcpuk-prod.prod.gcp.cloud.uk.hsbc',
+    updated_date = CURRENT_TIMESTAMP
+WHERE region = 'UK' AND platform = 'GCP' AND environment = 'PRD';
