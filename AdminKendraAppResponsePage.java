ad5418976a42841c4c24cb521f106b6e0b362399
Navneetpatid <patidarnavneet580@gmail.com>
2025-04-09 17:52:18 +0530

    Update AdminKendraAppResponsePage.java

--- AdminKendraAppResponsePage.java
+++ AdminKendraAppResponsePage.java
@@ -1,39 +1,8 @@
-package com.hsbc.hap.cer.model;
+package com.hsbc.hap.cer.service;
 
-public class QueryResult {
-    private String query;
-    private boolean success;
-    private String errorMessage;
+import com.hsbc.hap.cer.model.QueryResult;
+import java.util.List;
 
-    public QueryResult() {}
-
-    public QueryResult(String query, boolean success, String errorMessage) {
-        this.query = query;
-        this.success = success;
-        this.errorMessage = errorMessage;
-    }
-
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
-    }
+public interface HapCERService {
+    List<QueryResult> executeQueries(List<String> queries);
 }
