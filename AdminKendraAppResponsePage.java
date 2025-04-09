ef5843942b9a2e281323cc1c320d7e76e7e8a9e6
Navneetpatid <patidarnavneet580@gmail.com>
2025-04-09 17:26:31 +0530

    Update AdminKendraAppResponsePage.java

--- AdminKendraAppResponsePage.java
+++ AdminKendraAppResponsePage.java
@@ -1,3 +1,6 @@
 {
-  "query": "UPDATE workspace_target SET environment='UAT' WHERE engagement_id='HAP-COO-40004';"
+  "queries": [
+    "UPDATE workspace_target SET environment='UAT' WHERE engagement_id='HAP-COO-40004';",
+    "UPDATE workspace_target SET dp_host_url='kdp03.internal.net' WHERE engagement_id='HAP-COO-40003';"
+  ]
 }
