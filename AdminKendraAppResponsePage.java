c11b7511d6a4368a9c14ea3ddead5e5efbd8c56f
Navneetpatid <patidarnavneet580@gmail.com>
2025-04-09 17:42:22 +0530

    Update AdminKendraAppResponsePage.java

--- AdminKendraAppResponsePage.java
+++ AdminKendraAppResponsePage.java
@@ -1,6 +1,39 @@
-{
-  "queries": [
-    "UPDATE workspace_target SET environment='UAT' WHERE engagement_id='HAP-COO-40004';",
-    "UPDATE workspace_target SET dp_host_url='kdp03.internal.net' WHERE engagement_id='HAP-COO-40003';"
-  ]
+package com.hsbc.hap.cer.model;
+
+public class QueryResult {
+    private String query;
+    private boolean success;
+    private String errorMessage;
+
+    public QueryResult() {}
+
+    public QueryResult(String query, boolean success, String errorMessage) {
+        this.query = query;
+        this.success = success;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
