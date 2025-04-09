75e5dcb011cdcc9c76b177c40b3c18c45d5d7825
Navneetpatid <patidarnavneet580@gmail.com>
2025-04-09 17:57:13 +0530

    Update AdminKendraAppResponsePage.java

--- AdminKendraAppResponsePage.java
+++ AdminKendraAppResponsePage.java
@@ -1,28 +1,6 @@
-package com.hsbc.hap.cer.controller;
-
-import com.hsbc.hap.cer.model.QueryRequest;
-import com.hsbc.hap.cer.model.QueryResult;
-import com.hsbc.hap.cer.service.HapCERService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/hap/cer")
-public class CERController {
-
-    @Autowired
-    private HapCERService hapCerService;
-
-    @PostMapping("/bulkUpdate")
-    public ResponseEntity<?> bulkUpdate(@RequestBody QueryRequest queryRequest) {
-        try {
-            List<QueryResult> resultList = hapCerService.executeQueries(queryRequest.getQueries());
-            return ResponseEntity.ok(resultList);
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Server error: " + e.getMessage());
-        }
-    }
+{
+  "queries": [
+    "UPDATE engagement_target SET region='US' WHERE engagement_id='HAP-C00-4004';",
+    "UPDATE workspace_target SET dp_host_url='http://example.com' WHERE engagement_id='HAP-C00-4003';"
+  ]
 }
