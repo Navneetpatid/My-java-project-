fd3f7a416e213f4b8b661424b5af686d12a67853
Navneetpatid <patidarnavneet580@gmail.com>
2025-08-21 15:31:24 +0530

    Update AddKendra.java

--- AddKendra.java
+++ AddKendra.java
@@ -1,49 +1,23 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.HashMap;
-import java.util.Map;
+pipeline {
+    agent any
+    
+    environment {
+        // Store token in Jenkins credentials for security
+        API_TOKEN = credentials('SNOW_CREDENTIAL_UAT') 
+    }
 
-public class HapCDRServiceImpl implements HapCDRService {
-    private static final Logger LOGGER = LogManager.getLogger(HapCDRServiceImpl.class);
-
-    public String processKongCerRequest(KongCerRequest request) {
-        String response = "";
-        try {
-            LOGGER.info("Received request: {}", request);
-
-            EngagementTargetKong engagementTargetKong = requestResponseMapper.mapToEngagementTargetKong(request);
-            engagementTargetKongDao.save(engagementTargetKong);
-            LOGGER.info("Saved EngagementTargetKong successfully");
-
-            WorkspaceTargetDetails workspaceTargetDetails = requestResponseMapper.mapToWorkspaceTargetDetails(request);
-            workspaceTargetDetailsDao.save(workspaceTargetDetails);
-            LOGGER.info("Saved WorkspaceTargetDetails successfully");
-
-            EngagementPluginDetail engagementPluginDetail = requestResponseMapper.mapToEngagementPluginDetail(request);
-            engagementPluginDetailsDao.save(engagementPluginDetail);
-            LOGGER.info("Saved EngagementPluginDetail successfully");
-
-            CpMaster cpMaster = requestResponseMapper.mapToCpMaster(request);
-            cpMasterDetailsDao.save(cpMaster);
-            LOGGER.info("Saved CpMaster successfully");
-
-            engagementTargetKongDao.save(engagementTargetKong);
-            LOGGER.info("Re-saved EngagementTargetKong successfully");
-
-            // Creating the response JSON
-            Map<String, String> responseMap = new HashMap<>();
-            responseMap.put("message", "Kong data saved successfully");
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            response = objectMapper.writeValueAsString(responseMap);
-
-        } catch (Exception e) {
-            LOGGER.error("Error processing request: {}", e.getMessage(), e);
-            throw new ApplicationException(e.getMessage());
+    stages {
+        stage('Call GET API') {
+            steps {
+                script {
+                    sh """
+                        curl --request GET \
+                        'https://cluster1-test:1138/cdp/v1alpha/datamanagementid/HAPCQA0004&namespace=hap.hk' \
+                        --header 'Content-Type: application/json' \
+                        --header "X-HSBC-E2E-Trust-Token: ${API_TOKEN}"
+                    """
+                }
+            }
         }
-
-        LOGGER.info("Returning response: {}", response);
-        return response;
     }
 }
