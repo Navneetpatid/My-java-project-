3700d7ce1e8c68760a8aadd329dabe41e70ceea8
Navneetpatid <patidarnavneet580@gmail.com>
2025-08-21 16:19:43 +0530

    Update AddKendra.java

--- AddKendra.java
+++ AddKendra.java
@@ -1,41 +1,25 @@
 pipeline {
     agent any
-
     stages {
-        stage('Get Token') {
+        stage('Call API') {
             steps {
-                script {
-                    // Use Jenkins credentials
-                    withCredentials([usernamePassword(credentialsId: 'SNOW_CREDENTIAL_UAT',
-                                                     usernameVariable: 'SNOW_USER',
-                                                     passwordVariable: 'SNOW_PASS')]) {
-                        def response = sh(
-                            script: """
-                                curl --silent --request POST \\
-                                  'https://cmp-lb2b.dsp.prod.eu.systems.uk.hsbc:8443/dsp/rest-sts/DSP-1B2B/tokenTranslator?action=translate' \\
-                                  --header 'Content-Type: application/json' \\
-                                  --header 'cookie: AMToken=AAlTSOQMDIABHR5C...' \\
-                                  --data '{
-                                      "input_token_state": {
-                                          "token_type": "CREDENTIAL",
-                                          "username": "${SNOW_USER}",
-                                          "password": "${SNOW_PASS}"
-                                      },
-                                      "output_token_state": {
-                                          "token_type": "JWT"
-                                      }
-                                  }'
-                            """,
-                            returnStdout: true
-                        ).trim()
-
-                        echo "Full Response: ${response}"
-
-                        // Parse JSON (requires Pipeline Utility Steps plugin)
-                        def json = readJSON text: response
-                        def token = json?.token ?: "No token found"
-                        echo "Generated Token: ${token}"
-                    }
+                withCredentials([usernamePassword(credentialsId: 'SNOW_CREDENTIAL_UAT', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                    sh '''
+                        curl -v -k --request POST \
+                          "https://cmb-ib2b-dsp.pprod-eu.systems.uk.hsbc.com:8443/dsp/rest-stss/DSP-1B2B/tokenTranslator?action=translate" \
+                          --header "Content-Type: application/json" \
+                          --cookie "AMToken=AAJTSQACMDA..." \
+                          --data "{
+                            \\"input_token_state\\": {
+                              \\"token_type\\": \\"CREDENTIAL\\",
+                              \\"username\\": \\"$USER\\",
+                              \\"password\\": \\"$PASS\\"
+                            },
+                            \\"output_token_state\\": {
+                              \\"token_type\\": \\"JWT\\"
+                            }
+                          }"
+                    '''
                 }
             }
         }
