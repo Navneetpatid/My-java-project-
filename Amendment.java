d3e26bb9eca5b22aabdc33375f350327ab811efb
Navneetpatid <patidarnavneet580@gmail.com>
2025-08-21 12:11:29 +0530

    Update Amendment.java

--- Amendment.java
+++ Amendment.java
@@ -1,31 +1,45 @@
-package com.janaushadhi.adminservice.entity;
+pipeline {
+    agent any
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+    stages {
+        stage('Generate Token') {
+            steps {
+                script {
+                    // Call your token API
+                    def tokenResponse = sh(
+                        script: '''
+                        curl -s -X POST "http://localhost:8080/auth/token" \
+                        -H "Content-Type: application/json" \
+                        -d '{"username":"myuser","password":"mypassword"}'
+                        ''',
+                        returnStdout: true
+                    ).trim()
 
-import java.util.Date;
+                    // Extract token from JSON response (assumes {"token":"xyz"})
+                    def token = sh(
+                        script: """echo '${tokenResponse}' | jq -r '.token'""",
+                        returnStdout: true
+                    ).trim()
 
-@Data
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "amendment")
-public class Amendment {
+                    echo "Generated Token: ${token}"
 
-    @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+                    // Save in env for next stage
+                    env.AUTH_TOKEN = token
+                }
+            }
+        }
 
-    private  Long id;
-    private String tenderSubTitle;
-    private String publishDate;
-    private String closingDate;
-    private String tenderfile;
-    private String nitfile;
-    private String boqfile;
-    private Date createdDate;
-    private Date updatedDate;
-    private  Short status;
-
+        stage('Call GET API') {
+            steps {
+                script {
+                    sh """
+                    curl -s -X GET "http://localhost:8080/api/v1/data?managementId=HAP_CCO_40004&namespace=hap_kc_db" \
+                    -H "Content-Type: application/json" \
+                    -H "X-HSBC-TEST: Value" \
+                    -H "Trust-Token: ${env.AUTH_TOKEN}"
+                    """
+                }
+            }
+        }
+    }
 }
