c5c11e452824ef5da7c9b965f1f665c1dabc9bf3
Navneetpatid <patidarnavneet580@gmail.com>
2025-08-21 16:08:50 +0530

    Update AddKendra.java

--- AddKendra.java
+++ AddKendra.java
@@ -9,7 +9,7 @@ pipeline {
                                                       usernameVariable: 'SNOW_USER',
                                                       passwordVariable: 'SNOW_PASS')]) {
 
-                        // Write JSON request body to a temp file
+                        // Write request JSON body into file
                         writeFile file: 'request.json', text: """
                         {
                             "input_token_state": {
@@ -23,13 +23,13 @@ pipeline {
                         }
                         """
 
-                        // Execute curl with JSON file as input
+                        // Use corrected hostname
                         def response = sh(
                             script: """
                                 curl --silent --fail --show-error --request POST \\
-                                'https://cmp-lb2b.dsp.prod.eu.systems.uk.hsbc:8443/dsp/rest-sts/DSP-1B2B/tokenTranslator?action=translate' \\
+                                'https://cmp-lb2b.dsp.prod.eu.hsbc.systems:8443/dsp/rest-sts/DSP-1B2B/tokenTranslator?action=translate' \\
                                 --header 'Content-Type: application/json' \\
-                                --header 'Authorization: AJDJSDJCMODABJHS7GUAMDPXYAAQUZENAAJAwQJ0MAX01JXW01JLC06AX101JOT5F1IMWMToJUMWNTf0AJZddr6ZS16nZbG' \\
+                                --header 'Cookie: AMToken=AJDJSDJCMODABJHS7GUAMDPXYAAQUZENAAJAwQJ0MAX01JXW01JLC06AX101JOT5F1IMWMToJUMWNTf0AJZddr6ZS16nZbG' \\
                                 --data @request.json
                             """,
                             returnStdout: true
@@ -37,7 +37,7 @@ pipeline {
 
                         echo "Raw Response: ${response}"
 
-                        // If response is JSON, extract token
+                        // Parse JSON if response is JSON
                         def json = readJSON text: response
                         def token = json?.token ?: "No token found"
                         echo "Generated Token: ${token}"
