1591374163a1d36bcddc66f67a329cfcd6120157
Navneetpatid <patidarnavneet580@gmail.com>
2025-09-05 13:51:26 +0530

    Update AddKendraRepository.java

--- AddKendraRepository.java
+++ AddKendraRepository.java
@@ -59,3 +59,45 @@ def addShpData(def engagementId, def einId, def platform, def bgpF, def namespac
     }
     return returnVal
 }
+
+@Library('License_test@licenspec03') _ pipeline { agent any parameters { string(name: 'ENGAGEMENT_ID', defaultValue: 'HAP-COO-40010', description: 'Enter Engagement ID') string(name: 'EIN_ID', defaultValue: '1237', description: 'Enter EIN ID') string(name: 'PLATFORM', defaultValue: 'IKP', description: 'Enter Platform') string(name: 'BGPF', defaultValue: 'COO IT', description: 'Enter BGPF') string(name: 'NAMESPACE', defaultValue: 'hap-hk-cto-113', description: 'Enter Namespace') string(name: 'CLUSTER', defaultValue: 'ClusterTest123', description: 'Enter Cluster') string(name: 'ENVIRONMENT', defaultValue: 'DEV', description: 'Enter Environment') string(name: 'PROJECT', defaultValue: 'testProject1245', description: 'Enter Project') string(name: 'REGION', defaultValue: 'UK', description: 'Enter Region') string(name: 'PROXY', defaultValue: '1345', description: 'Enter Proxy') string(name: 'DEPLOY_UTILITY_NAME', defaultValue: 'deploy123', description: 'Enter Deploy Utility Name') string(name: 'HELM_CHART_NEXUS_URL', defaultValue: 'https://anypoint.mulesoft.com', description: 'Enter Helm Chart Nexus URL') string(name: 'GCP_NODE', defaultValue: 'node1234', description: 'Enter GCP Node') }
+
+environment {
+    TOKEN = credentials('SNOW_CREDENTIAL_UAT') // Jenkins Credential ID
+}
+
+stages {
+    stage('Generate ServiceNow Token') {
+        steps {
+            script {
+                env.API_TOKEN = "${TOKEN}"
+            }
+        }
+    }
+
+    stage('Fetch Add SHP Data') {
+        steps {
+            script {
+                def result = kong.addShpData(
+                    params.ENGAGEMENT_ID,
+                    params.EIN_ID,
+                    params.PLATFORM,
+                    params.BGPF,
+                    params.NAMESPACE,
+                    params.CLUSTER,
+                    params.ENVIRONMENT,
+                    params.PROJECT,
+                    params.REGION,
+                    params.PROXY,
+                    params.DEPLOY_UTILITY_NAME,
+                    params.HELM_CHART_NEXUS_URL,
+                    params.GCP_NODE
+                )
+            }
+        }
+    }
+}
+
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
