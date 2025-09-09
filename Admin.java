91d8e0f4eac3c777670e47a9e335fedd64064fbb
Navneetpatid <patidarnavneet580@gmail.com>
2025-09-09 16:17:28 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,2 +1,8 @@
-<b>Note:</b> This parameter is applicable only if deploy utility is <b>Helm</b>.<br>
-Example: https://nexus.company.com/repository/helm/
+def statusCode = sh(script: """#!/bin/bash
+  set -e
+  curl -s -o ${fileName} -w "%{http_code}" \
+    -H "${contentType}" \
+    -H "X-HSBC-E2E-Trust-Token: ${token}" \
+    -X POST "${url}" \
+    --data @kong_request.json
+""", returnStdout: true).trim()
