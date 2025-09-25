f3932f25723dd0efc75baaa2a876d4adc2769fe9
Navneetpatid <patidarnavneet580@gmail.com>
2025-09-25 14:59:27 +0530

    Update AddKendraServiceImplTest.java

--- AddKendraServiceImplTest.java
+++ AddKendraServiceImplTest.java
@@ -1,33 +1,58 @@
-import Logger   // make sure Logger.groovy is available in vars/ or src/
+import groovy.util.*
+import groovy.json.*
+import groovy.lang.*
+
+@Field def logger = new Logger()
 
 def call() {
     node {
         try {
-            stage('Print Parameters') {
-                Logger.info("Engagement ID: ${params.engagementid}")
-                Logger.info("EMI ID: ${params.emid}")
-                Logger.info("URL: ${params.URL}")
-                Logger.info("Email ID: ${params.Email_Id}")
-            }
-
-            stage('Split and Print URL') {
-                def parts = splitUrl(params.URL)
-                Logger.debug("Splitting URL: ${params.URL}")
-                Logger.info("URL Parts:")
-                parts.each { part ->
-                    Logger.trace("Part => ${part}")
-                }
-            }
-
             stage('Send Email') {
-                def emailBody = """
-                    <h3>Pipeline Execution Details</h3>
-                    <ul>
-                        <li><b>ENGAGEMENT_ID:</b> ${params.engagementid}</li>
-                        <li><b>MANDATORY_PLUGIN:</b> ${params.emid}</li>
-                        <li><b>REGION:</b> ${params.URL}</li>
-                    </ul>
-                """
+                def emailBody = """\
+<h3>Pipeline Execution Details</h3>
+<ul>
+  <li><b>ENGAGEMENT_ID:</b> ${params.engagementid}</li>
+  <li><b>MANDATORY_PLUGIN:</b> ${params.emid}</li>
+  <li><b>REGION:</b> ${params.URL}</li>
+</ul>
+
+<h3>AAP Execution Using HAP Deployment Service</h3>
+<p>Please use the below package ID PURL in the Release Task:</p>
+<table border="1" style="border-collapse: collapse; width: 100%;">
+  <tr>
+    <th style="padding: 8px; text-align: left;">Field</th>
+    <th style="padding: 8px; text-align: left;">Value</th>
+  </tr>
+  <tr>
+    <td style="padding: 8px;">PURL</td>
+    <td style="padding: 8px;">${params.purl}</td>
+  </tr>
+</table>
+
+<h3>Use the below values while running the Production Pipeline:</h3>
+<table border="1" style="border-collapse: collapse; width: 100%;">
+  <tr>
+    <th style="padding: 8px; text-align: left;">Field</th>
+    <th style="padding: 8px; text-align: left;">Value</th>
+  </tr>
+  <tr>
+    <td style="padding: 8px;">Release Nexus Version</td>
+    <td style="padding: 8px;">${params.releaseNexusVersionUrl}</td>
+  </tr>
+  <tr>
+    <td style="padding: 8px;">Rollback Nexus Version</td>
+    <td style="padding: 8px;">${params.rollbackNexusVersionUrl}</td>
+  </tr>
+</table>
+
+<p>Please contact HAP SUPPORT at 
+<a href="mailto:hap.support@hsbc.co.in">hap.support@hsbc.co.in</a> 
+for any query.</p>
+
+<p>Refer to the following link for more help: 
+<a href="https://alm-confluence.systems.uk.hsbc/confluence/display/ENGP/HAP+Deployment+Service+-+CD+Pre-Prod+and+Prod">
+HAP Deployment Service Documentation</a></p>
+"""
 
                 emailext(
                     to: "${params.Email_Id}",
@@ -36,16 +61,19 @@ def call() {
                     mimeType: 'text/html'
                 )
 
-                Logger.info("Email sent successfully to ${params.Email_Id}")
+                logger.info("Email sent successfully to ${params.Email_Id}")
             }
-
         } catch (Exception e) {
-            Logger.error("Error while running TestPipeline: ${e.message}")
+            logger.error("Error while running TestPipeline: ${e.message}")
             error("Pipeline failed: ${e.message}")
         }
     }
 }
 
 def splitUrl(String url) {
-    return url.tokenize(".")
-    }
+    return url.tokenize("/")
+}
+
+def splitemid(String emid) {
+    return emid.tokenize("-")
+                    }
