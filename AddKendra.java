37f8615e96c5dde43f154831c1516b25eb801b08
Navneetpatid <patidarnavneet580@gmail.com>
2026-02-19 14:22:27 +0530

    Update AddKendra.java

--- AddKendra.java
+++ AddKendra.java
@@ -8,3 +8,11 @@ This will help us ensure proper coordination and smooth issue resolution.
 Thank you for your continued support.
 Best Regards,
 Navneet
+
+  As I am part of the HAP support team, I kindly request that I be informed promptly of any upcoming or ongoing HAP-related issues. Please ensure that I am included in all relevant discussions and communications so that I can actively contribute and provide timely support.
+If any issue is identified and I am not involved initially, I would appreciate it if you could share the complete details with me after your review, including the impact analysis and resolution steps taken.
+This will help us maintain proper alignment, ensure smooth coordination, and enhance the overall efficiency of issue resolution.
+Thank you for your continued support and cooperation.
+Best Regards,
+Navneet
+
