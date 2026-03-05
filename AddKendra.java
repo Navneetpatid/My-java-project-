8fe5e0d89419165ece2cd02fa8c429e5afde9bc3
Navneetpatid <patidarnavneet580@gmail.com>
2026-03-05 14:37:41 +0530

    Update AddKendra.java

--- AddKendra.java
+++ AddKendra.java
@@ -14,3 +14,6 @@ Enhancements for control 5.1 to support staff IDs starting with CP (To support I
 Enhancements to Mule 4 Nexus artefacts download Module
 IKP Platform component deployment Feature to support pilot onboarding
 Enhancements to Kong DMZ Deployments
+
+  This change is being implemented to promote ITADS Deployment Service Release 3.3.2 to the Production environment. The release includes platform enhancements, configuration updates, and deployment improvements. All changes have been successfully tested and validated in the UAT environment before the production release.
+
